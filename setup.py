83b65a9389057745612a21802999b35c3a81733c
Henning Hraban Ramm <hraban@fiee.net>
2017-05-19 17:49:47 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 METADATA = dict(
     name='django-userpreferences',
-    version='0.1.4',
+    version='0.1.5',
     author='Nicolas Patry et al.',
     author_email='nicolas.patry@centraliens.net',
 
