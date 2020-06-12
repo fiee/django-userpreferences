a7dc497eb89cd9f939733df52c3c38d65b43856d
hraban <hraban@fiee.net>
2020-06-12 17:23:34 +0200

    modernize tests

--- preferences/tests/runtests.py
+++ preferences/tests/runtests.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import os
 import sys
-from django.test.simple import DjangoTestSuiteRunner
-# from django.conf import settings
+import django
+from django.conf import settings
+from django.test.utils import get_runner
 
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'test_settings'
-parent = os.path.dirname(os.path.dirname(os.path.dirname(
-                         os.path.abspath(__file__))))
-
-sys.path.insert(0, parent)
-
-
-def runtests():
-    DjangoTestSuiteRunner(failfast=False).run_tests([
-        'preferences.PreferencesAPITest',
-        ], verbosity=1, interactive=True)
-
-
-if __name__ == '__main__':
-    runtests()
+if __name__ == "__main__":
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["tests"])
+    sys.exit(bool(failures))
--- preferences/tests/test_settings.py
+++ preferences/tests/test_settings.py
@@ -8,6 +8,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.sessions',
     'django.contrib.contenttypes',
+    'django.contrib.messages',
     'django.contrib.admin',
     'preferences',
 )
@@ -18,12 +19,38 @@ DATABASES = {
     }
 }
 
-MIDDLEWARE_CLASSES = (
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware', # first makes sense
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': (
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                #'django.template.context_processors.i18n',
+                'django.contrib.messages.context_processors.messages',
+                #'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                #'django.template.context_processors.tz'
+            ),
+        },
+    },
+]
+
 
 SECRET_KEY = 'testsecretkey'
 
